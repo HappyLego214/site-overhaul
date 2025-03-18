53cf97a1db33873bc9c927236585fe0a8e6fa606
josh <anthony21josh@gmail.com>
2025-03-18 15:23:05 +0800

    adds first frame and changes to css styling on main layout

---
+++ src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className="flex-row">
+        <div className="technology-background"></div>
+        <div className="regroup">
+            <span className="home">Home</span>
+            <span className="solutions">Solutions</span>
+            <span className="stories">Stories</span>
+            <span className="about">About</span>
+            <span className="contact">Contact</span>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
