7e290db8c12c44e044a166a9ebd609194e4fbb88
josh <anthony21josh@gmail.com>
2025-03-31 15:55:02 +0800

    ensured vite.config.ts file points to dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets are loaded relative to the current path
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
 })
